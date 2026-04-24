3a178946f1a3eea230cbfaed6e272076d93e8efe
Ryan Attard <ryanattard@gmail.com>
2026-04-24 13:21:04 +0200

    symmetric

---
+++ SWD62ASecurity2026/Presentation/Controllers/CryptographyController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.Utilities;
+using System.Security.Cryptography;
+using System.Text;
+namespace Presentation.Controllers
+{
+    public class CryptographyController : Controller
+    {
+        public IActionResult TestSymmetric()
+        { 
+            SymmetricEncryptionHelper helper = new SymmetricEncryptionHelper();
+
+            string dataToEncrypt= "This is some data that I want to encrypt";
+
+            var myParams = helper.GenerateParameters(2, Aes.Create(), "p@$$w0rd");
+
+            byte[] dataToEncryptAsBytes = Encoding.UTF8.GetBytes(dataToEncrypt);
+
+            byte[] cipher =
+                helper.Encrypt(Aes.Create(), myParams, dataToEncryptAsBytes);
+
+
+
+            byte[] originalValue =   helper.Decrypt(Aes.Create(), myParams, cipher);
+
+            string originalValueAsString = Encoding.UTF8.GetString(originalValue);  
+
+            return Content($"Cipher is {Convert.ToBase64String(cipher)} Original Value is {Encoding.UTF8.GetString(originalValue)}");
+        }
+
+       
+    }
+}
---
+++ SWD62ASecurity2026/Presentation/Utilities/SymmetricEncryptionHelper.cs
@@ -0,0 +1,106 @@
+﻿using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Presentation.Utilities
+{
+
+    public class SymmetricParameters
+    {
+        public byte[] Key { get; set; }
+        public byte[] IV { get; set; }
+    }
+
+    public class SymmetricEncryptionHelper
+    {
+        public SymmetricParameters GenerateParameters(int choiceOfGeneration, SymmetricAlgorithm algorithm, string? password)
+        {
+
+            SymmetricParameters parameters = new SymmetricParameters();
+            
+            switch(choiceOfGeneration)
+            {
+                case 0:
+                    //Approach 1: you randomly generate a number of bytes for the key and IV
+                    parameters.Key = new byte[algorithm.KeySize / 8];
+                    for (int i = 0; i < parameters.Key.Length; i++)
+                    {
+                        parameters.Key[i] = (byte)i;
+                    }
+
+                    parameters.IV = new byte[algorithm.BlockSize / 8];
+                    for (int i = 0; i < parameters.IV.Length; i++)
+                    {
+                        parameters.IV[i] = (byte)i;
+                    }
+
+                    break;
+
+                case 1:
+                    //approach 2: you use the algorithm instance to generate the key and IV for you
+                    algorithm.GenerateKey(); algorithm.GenerateIV();
+                    parameters.Key = algorithm.Key;
+                    parameters.IV = algorithm.IV;
+
+                    break;
+
+                case 2:
+                    //approach 3: you can use a helper class like Rfc2898DeriveBytes to generate the key and IV for you based on a password and salt
+                    Rfc2898DeriveBytes rfc2898DeriveBytes
+                        = new Rfc2898DeriveBytes(
+                            Encoding.UTF8.GetBytes(password ?? string.Empty), //human generated input
+                            new byte[] { 23, 45, 90, 150, 250, 230, 201, 190, 43, 45, 65, 23 },
+                            1000,
+                            HashAlgorithmName.SHA256
+                            );
+
+                    parameters.Key = rfc2898DeriveBytes.GetBytes(algorithm.KeySize / 8);
+                    parameters.IV = rfc2898DeriveBytes.GetBytes(algorithm.BlockSize / 8);
+                    break;
+            }
+            return parameters;
+        }
+
+
+        public byte[] Encrypt(SymmetricAlgorithm alg, SymmetricParameters paramters, byte[] data)
+        {
+            alg.Key = paramters.Key;
+            alg.IV = paramters.IV;
+
+            //Memorystream its just a stream of data which holds your data temporarily in memory
+            MemoryStream outputStream = new MemoryStream();
+            
+            MemoryStream inputStream = new MemoryStream(data);
+            inputStream.Position = 0;
+
+            using (CryptoStream cryptoStream =
+                new CryptoStream(inputStream, alg.CreateEncryptor(), CryptoStreamMode.Read))
+            {
+                cryptoStream.CopyTo(outputStream);
+            }
+            outputStream.Position = 0;
+            return outputStream.ToArray();
+        }
+
+        public byte[] Decrypt(SymmetricAlgorithm alg, SymmetricParameters parameters, byte[] cipher)
+        {
+            alg.Key = parameters.Key;
+            alg.IV = parameters.IV;
+
+            //Memorystream its just a stream of data which holds your data temporarily in memory
+            MemoryStream outputStream = new MemoryStream();
+
+            MemoryStream inputStream = new MemoryStream(cipher);
+            inputStream.Position = 0;
+
+            using (CryptoStream cryptoStream =
+                new CryptoStream(inputStream, alg.CreateDecryptor(), CryptoStreamMode.Read))
+            {
+                cryptoStream.CopyTo(outputStream);
+            }
+            outputStream.Position = 0;
+            return outputStream.ToArray();
+        }
+    }
+}
